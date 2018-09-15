cd5f940f7d790720acdf2f6964a2d50a7ee82e1a
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 12:05:19 +0700

    Props

--- 06. ReactJs/app.jsx
+++ 06. ReactJs/app.jsx
@@ -1,60 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            header: 'This is the Header',
-            content: 'This is the Content',
-            footer: 'This is the Footer'
-        };
-    }
-
-
-
     render() {
         return (
             <div>
                 <div>
-                    <h1>{this.state.header}</h1>
+                    <h1>{this.props.header}</h1>
                 </div>
-                <div>{this.state.content}</div>
-                <div>{this.state.footer}</div>
+                <div>{this.props.content}</div>
+                <div>{this.props.footer}</div>
             </div>
         );
     }
 }
-
-/*class Header extends App {
-    render() {
-        return (
-            <div>
-                This is the Header
-            </div>
-        );
-    }
-}
-
-class Content extends App {
-    render() {
-        return (
-            <div>
-                This is the Content
-            </div>
-        );
-    }
-}
-
-class Footer extends App {
-    render() {
-        return (
-            <div>
-                This is the Footer
-            </div>
-        );
-    }
-}*/
 
 export default App;
--- 06. ReactJs/main.js
+++ 06. ReactJs/main.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './app.jsx';
 
-ReactDom.render( <App />, document.getElementById('app'));
+ReactDom.render( <App
+    header={"This is the header"}
+    content={"This is the content"}
+    footer={"This is the footer"}
+/>, document.getElementById('app'));

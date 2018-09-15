626eb1973b6d6e1f58db58231fb2c4a50512669d
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 19:16:27 +0700

    Laravel Blog PHP Configurations

--- 07.Laravel-Blog/config/services.php
+++ 07.Laravel-Blog/config/services.php
@@ -35,4 +35,8 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'braintree' => [
+        'secret' => env('SECRET_KEY')
+    ]
+
 ];
--- 07.Laravel-Blog/routes/web.php
+++ 07.Laravel-Blog/routes/web.php
@@ -3,3 +3,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+//    echo Config::get('services.braintree.secret');
+    echo Config::get('services.mailgun.secret');
+});

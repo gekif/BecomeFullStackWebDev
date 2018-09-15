b64d1eb6efd314dc7a062f78113be470480d1078
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 11:29:54 +0700

    Making The First React App

---
+++ 06. ReactJs/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                We're Learning ReactJS
+            </div>
+        );
+
+
+    }
+}
+
+export default App;

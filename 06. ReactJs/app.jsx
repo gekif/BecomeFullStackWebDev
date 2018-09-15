3942de13eb3426b97cddd6d5c84875a8ceb5c561
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 12:35:10 +0700

    setState

--- 06. ReactJs/app.jsx
+++ 06. ReactJs/app.jsx
@@ -1,50 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
+    constructor(){
+        super();
 
         this.state = {
-            header: 'This is the Header',
-            content: 'This is the Content',
-            footer: 'This is the Footer',
+            data: []
         };
+
+        this.stateHandler = this.stateHandler.bind(this);
+    }
+
+    stateHandler() {
+        let item = 'The state';
+        let dataArray = this.state.data;
+        dataArray.push(item);
+        this.setState({data: dataArray});
     }
 
     render() {
-        return (
+        return(
             <div>
-                <Header propsHeader={this.state.header}/>
-                <Content propsContent={this.state.content}/>
-                <Footer propsFooter={this.state.footer}/>
+                <div>{this.state.data}</div>
+                <button onClick={this.stateHandler}>Click Here To Update</button>
             </div>
-        );
-    }
-}
-
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.propsHeader}</h1>
-            </div>
-        );
-    }
-}
-
-class Content extends React.Component {
-    render() {
-        return (
-            <div>{this.props.propsContent}</div>
-        );
-    }
-}
-
-class Footer extends React.Component {
-    render() {
-        return (
-            <div>{this.props.propsFooter}</div>
         );
     }
 }

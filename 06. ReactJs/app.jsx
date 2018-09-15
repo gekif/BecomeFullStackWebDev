aac2f22cbfe6cd1c588f6d99c50902e6cddf072e
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 11:46:32 +0700

    What is JSX

--- 06. ReactJs/app.jsx
+++ 06. ReactJs/app.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
+        let style = {
+            backgroundColor: 'blue',
+            color: 'white',
+            fontSize: '40px'
+        };
+
+        let var1 = 8;
+
         return (
             <div>
                 We're Learning ReactJS
+                <div data-shahattr="value">A New Div</div>
+                <span style={style}>Hello World</span>
+                <div>{4 + 4}</div>
+                <div>{var1 === 7 ? 'True' : 'False'}</div>
             </div>
         );
-
-
     }
 }
 

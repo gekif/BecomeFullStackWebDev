0e8906888539b15ee9c001135a86ea0e1d65d299
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 11:58:12 +0700

    State in React

--- 06. ReactJs/app.jsx
+++ 06. ReactJs/app.jsx
@@ -1,25 +1,60 @@
 import React from 'react';
 
 class App extends React.Component {
-    render() {
-        let style = {
-            backgroundColor: 'blue',
-            color: 'white',
-            fontSize: '40px'
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            header: 'This is the Header',
+            content: 'This is the Content',
+            footer: 'This is the Footer'
         };
+    }
 
-        let var1 = 8;
 
+
+    render() {
         return (
             <div>
-                We're Learning ReactJS
-                <div data-shahattr="value">A New Div</div>
-                <span style={style}>Hello World</span>
-                <div>{4 + 4}</div>
-                <div>{var1 === 7 ? 'True' : 'False'}</div>
+                <div>
+                    <h1>{this.state.header}</h1>
+                </div>
+                <div>{this.state.content}</div>
+                <div>{this.state.footer}</div>
             </div>
         );
     }
 }
+
+/*class Header extends App {
+    render() {
+        return (
+            <div>
+                This is the Header
+            </div>
+        );
+    }
+}
+
+class Content extends App {
+    render() {
+        return (
+            <div>
+                This is the Content
+            </div>
+        );
+    }
+}
+
+class Footer extends App {
+    render() {
+        return (
+            <div>
+                This is the Footer
+            </div>
+        );
+    }
+}*/
 
 export default App;

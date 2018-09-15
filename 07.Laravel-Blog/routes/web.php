e2ecc6e86ace67e6ffb32926719025c70759925e
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 19:05:20 +0700

    Laravel Blog PHP App Key

--- 07.Laravel-Blog/routes/web.php
+++ 07.Laravel-Blog/routes/web.php
@@ -1,41 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');*/
-
-
-Route::get('home', function () {
-
-});
-
-Route::post('home', function () {
-
-});
-
-Route::delete('home', function () {
-
-});
-
-Route::put('home', function () {
-
-});
-
-Route::view('home', function () {
-
 });

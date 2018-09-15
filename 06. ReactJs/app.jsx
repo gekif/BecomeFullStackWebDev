b6f3724d301fdf394d75d7b19689ea8a373b367f
Gekif <dzulfikar.maulana@gmail.com>
2018-09-15 12:23:26 +0700

    Props and State Combined

--- 06. ReactJs/app.jsx
+++ 06. ReactJs/app.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            header: 'This is the Header',
+            content: 'This is the Content',
+            footer: 'This is the Footer',
+        };
+    }
+
     render() {
         return (
             <div>
-                <div>
-                    <h1>{this.props.header}</h1>
-                </div>
-                <div>{this.props.content}</div>
-                <div>{this.props.footer}</div>
+                <Header propsHeader={this.state.header}/>
+                <Content propsContent={this.state.content}/>
+                <Footer propsFooter={this.state.footer}/>
             </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.propsHeader}</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>{this.props.propsContent}</div>
+        );
+    }
+}
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>{this.props.propsFooter}</div>
         );
     }
 }

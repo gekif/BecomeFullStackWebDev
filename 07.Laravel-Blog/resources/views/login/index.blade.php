1c9e07a449d427a29aa86acb05bc8ce8ea03296d
Gekif <dzulfikar.maulana@gmail.com>
2018-09-16 08:53:04 +0700

    Laravel Blog Keeping Old Data

--- 07.Laravel-Blog/resources/views/login/index.blade.php
+++ 07.Laravel-Blog/resources/views/login/index.blade.php
@@ -25,15 +25,15 @@
 
                 <div class="col-md-6 mb-3">
                     <label for="username">Email:</label>
-                    <input type="text" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" id="email" placeholder="Email">
+                    <input type="text" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" id="email" placeholder="Email" value="{{ old('email') }}">
                     <div class="invalid-feedback">
                         {{ $errors->first('email') }}
                     </div>
                 </div>
 
                 <div class="col-md-6 mb-3">
-                    <label for="password   ">Password:</label>
-                    <input type="text" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" id="password" placeholder="Password">
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" id="password" placeholder="Password">
                     <div class="invalid-feedback">
                         {{ $errors->first('password') }}
                     </div>

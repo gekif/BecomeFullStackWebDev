2e202cc250b8693c42301c926ac7bcef89189683
Gekif <dzulfikar.maulana@gmail.com>
2018-09-18 22:17:16 +0700

    TokoFikar 1.5.1: Edit dan Detil produk revisi minor

--- TokoFikar/resources/views/admin/products/details.blade.php
+++ TokoFikar/resources/views/admin/products/details.blade.php
@@ -10,8 +10,8 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="header">
-                    <h4 class="title">Product Detail</h4>
-                    <p class="category">List of all stock</p>
+                    <h4 class="title">Detil Produk</h4>
+                    <p class="category">Daftar Semua Stok</p>
                 </div>
                 <div class="content table-responsive table-full-width">
                     <table class="table table-striped">

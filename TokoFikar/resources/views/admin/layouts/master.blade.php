ad58a5d7219de9ca10ad63f86662ce7d539e3a35
Gekif <dzulfikar.maulana@gmail.com>
2018-09-19 19:57:44 +0700

    TokoFikarV2.0: Inisiasi Login

--- TokoFikar/resources/views/admin/layouts/master.blade.php
+++ TokoFikar/resources/views/admin/layouts/master.blade.php
@@ -10,14 +10,12 @@
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
     <meta name="viewport" content="width=device-width"/>
 
-
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="/assets/css/animate.min.css" rel="stylesheet" />
-    <link href="/assets/css/paper-dashboard.css" rel="stylesheet" />
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Muli:400,300" rel="stylesheet" type="text/css" />
-    <link href="/assets/css/themify-icons.css" rel="stylesheet" />
-    <link href="/assets/css/style.css" rel="stylesheet" />
+    <link href="{{ url('assets/css/bootstrap.min.css') }}" rel="stylesheet"/>
+    <link href="{{ url('assets/css/animate.min.css') }}" rel="stylesheet"/>
+    <link href="{{ url('assets/css/paper-dashboard.css') }}" rel="stylesheet"/>
+    <link href="{{ url('http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ url('https://fonts.googleapis.com/css?family=Muli:400,300') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('assets/css/themify-icons.css') }}" rel="stylesheet">
 
 </head>
 <body>
--- TokoFikar/routes/web.php
+++ TokoFikar/routes/web.php
@@ -3,6 +3,19 @@
 // Dashboard
 Route::get('/', 'DashboardController@index');
 
-//Products
+// Products
 Route::resource('/products', 'ProductController');
-//Route::get('/products', 'ProductController');
+
+
+// Orders
+Route::resource('/orders', 'OrderController');
+Route::get('/confirm/{id}', 'OrderController@confirm');
+Route::get('/pending/{id}', 'OderController@pending');
+
+// Users
+Route::resource('/users', 'UserController');
+
+// Admin Login
+Route::get('/admin/login', function () {
+    return view('admin.login');
+});

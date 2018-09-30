21d747304b08fc9461e91f7ae56cc35740b8843c
Gekif <dzulfikar.maulana@gmail.com>
2018-09-30 08:19:02 +0700

    TokoFikarV4.7: User Order Details

--- TokoFikar/app/Http/Controllers/front/UserProfileController.php
+++ TokoFikar/app/Http/Controllers/front/UserProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\front;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Order;
 
 class UserProfileController extends Controller
 {
@@ -19,6 +20,7 @@ class UserProfileController extends Controller
 
     public function show($id)
     {
-
+        $order = Order::find($id);
+        return view('front.profile.details', compact('order'));
     }
 }
